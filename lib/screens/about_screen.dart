866148c16c16aa629ca5a23473cfb4f832b50adf
B C Sagar <62722608+99bcsagar@users.noreply.github.com>
2020-11-08 08:56:13 +0530

    Update about_screen.dart

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -84,7 +84,7 @@ class About extends StatelessWidget {
                 margin: EdgeInsets.only(left: 20, right: 20),
                 child: Text(
                   "We are the Geeks of Siddaganga Institute of Technology. We are powered by GeeksforGeeks Organization. We Help Students in the field of Computer Science and Artificial Intelligence in their path to potential placement and Higher Education. Our Alumni Work at Google, Microsoft, Amazon, Facebook, UBER, Cisco, Adobe, Media.net, Tower Research and many other companies",
-                  style: TextStyle(color: Color(0xFF2F8D46),
+                  style: TextStyle(color: Color(0xFFFFFFFF),
                                    fontSize: 18),
                   textAlign: TextAlign.start,
                 )),
