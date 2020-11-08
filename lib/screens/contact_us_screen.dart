179bed96b6dbbbc56aa9f6d41d82a3d04cb2bfe6
B C Sagar <62722608+99bcsagar@users.noreply.github.com>
2020-11-08 09:32:15 +0530

    Update contact_us_screen.dart

--- lib/screens/contact_us_screen.dart
+++ lib/screens/contact_us_screen.dart
@@ -48,7 +48,7 @@ class EmbeddedMaps extends StatelessWidget {
                   EmbeddedMap(),
                 ],
               )
-            ],
+            
           ),
         ),
       ),
